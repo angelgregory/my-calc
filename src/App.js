7f549d2c51d3bcfb1463229c1a9efdca10255c21
angelgregory <aglansangan@gmail.com>
2023-01-30 15:19:39 +0800

    working display

--- src/App.js
+++ src/App.js
@@ -1,58 +1,65 @@
-import logo from "./logo.svg";
+
+import { useState } from "react";
+
 import "./App.css";
 
-function CalcButton({ buttonLabel = "?" }) {
-   function handlePlayClick() {
-      alert(`${buttonLabel}`);
-   }
+
+function CalcButton({ buttonLabel = "?", onClick }) {
    return (
       <div>
-         <button onClick={handlePlayClick}>{buttonLabel}</button>
+         <button onClick={onClick}>{buttonLabel}</button>
       </div>
    );
 }
-function CalcScreen({ log = "?" }) {
+function CalcScreen({ display = "?" }) {
    return (
       <div>
-         <h1>{log}</h1>
+         <h1>{display}</h1>
       </div>
    );
-}
-function handleClick() {
-   alert("You clicked me!");
 }
 
 function App() {
+   
+  const [disp, setDisp] = useState(0);
+  const numberClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    setDisp(value);
+    console.log(value);
+    // alert(value);
+  };
+
    return (
       <div className="App">
          <div className="calculator">
             <div className="screen-log">
-               <CalcScreen log="0" />
+               <CalcScreen display={disp} />
             </div>
             <div className="button-section">
                <div className="layers">
-                  <CalcButton buttonLabel="OR" />
-                  <CalcButton buttonLabel="0" />
-                  <CalcButton buttonLabel="=" />
-                  <CalcButton buttonLabel="+" />
+                  <CalcButton buttonLabel="CLR" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="0" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="=" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="+" onClick={numberClickHandler} />
                </div>
                <div className="layers">
-                  <CalcButton buttonLabel="1" />
-                  <CalcButton buttonLabel="2" />
-                  <CalcButton buttonLabel="3" />
-                  <CalcButton buttonLabel="-" />
+                  <CalcButton buttonLabel="1" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="2" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="3" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="-" onClick={numberClickHandler} />
                </div>
                <div className="layers">
-                  <CalcButton buttonLabel="4" />
-                  <CalcButton buttonLabel="5" />
-                  <CalcButton buttonLabel="6" />
-                  <CalcButton buttonLabel="x" />
+                  <CalcButton buttonLabel="4" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="5" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="6" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="x" onClick={numberClickHandler} />
                </div>
                <div className="layers">
-                  <CalcButton buttonLabel="7" />
-                  <CalcButton buttonLabel="8" />
-                  <CalcButton buttonLabel="9" />
-                  <CalcButton buttonLabel="÷" />
+                  <CalcButton buttonLabel="7" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="8" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="9" onClick={numberClickHandler} />
+                  <CalcButton buttonLabel="÷" onClick={numberClickHandler} />
                </div>
             </div>
          </div>
